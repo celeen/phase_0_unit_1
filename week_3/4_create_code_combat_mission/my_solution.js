b7363f72e35daadbf19126cddb2918024294c186
Celeen Rusk <celeenrusk@gmail.com>
2014-04-18 15:26:55 -0500

    4 changes

--- week_3/4_create_code_combat_mission/my_solution.js
+++ week_3/4_create_code_combat_mission/my_solution.js
@@ -1,18 +1,38 @@
-// I worked [by myself, with:] on this challenge
+// I worked [myself] on this challenge
 
 // Your mission description:
-// 
+// Rapunzel is in her tower, which is located 8m east and 2m north of the opening to a clearing in the woods. Her tower is 50m high, and her hair is 49m  long. Tharin enters the clearing at its entrance, (0,0). He must walk to the tower,call out to her to get her to let down her hair, and climb her hair.
 // 
 // 
 
 // Pseudocode
-// 
-// 
-// 
-// 
-// 
+// create an object, Tharin.
+// give tharin a startingLocation property of (0,0), and a height property of 1m.
+// create an object tower
+// give tower a height property of 50m, a location property of (2,8), and a climbable property set to 0.
+// create an object rapunzel.
+// give rapunzel a climbableHair property of 49m, and a location property of (2,8), and a height peroprty 50m
+// create a function moveUp that moves tharin 1m north (up).
+// create a function moveRight that moves Tharin 1m east (right).
+// create a function callOut which adds the value of rapunzel's climbableHair property to the tower's climbable property.
+// create a function climb which allows Tharin to climb anything with a height property equal to or greater than his own.
 
 // Initial Code
+var tharin = {}
+tharin.location = (0,0);
+tharin.height = 1;
+
+var tower = {}
+tower.height = 50;
+tower.location = (2,8);
+tower.climbable = 0;
+
+var rapunzel = {}
+rapunzel.climbableHair = 49;
+rapunzel.location = (2,8);
+rapunzel.height = 50;
+
+
 
 
 
